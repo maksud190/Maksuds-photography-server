ae4d0a67451dfd158d4ab8d52d5c4209be72d081
maksud190 <maknep3rdoct@gmail.com>
2022-11-08 23:23:13 +0600

    services api created

--- index.js
+++ index.js
@@ -12,15 +12,25 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xngqds2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const serviceCollection = client.db('m-photography').collection('services');
 
+        app.get('/services', async(req, res)=> {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=> console.error(err))
 
 
 app.get('/', (req, res)=> {
