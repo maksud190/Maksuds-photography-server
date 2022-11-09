12e2356f2e1a30eae1c5d8dff364d908413956a8
maksud190 <maknep3rdoct@gmail.com>
2022-11-09 20:50:09 +0600

    selected service api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,29 +14,36 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xngqds2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db('m-photography').collection('services');
 
-        app.get('/services', async(req, res)=> {
+        app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const selectedService = await serviceCollection.findOne(query);
+            res.send(selectedService);
+        })
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch(err=> console.error(err))
+run().catch(err => console.error(err))
 
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Maksuds Photography server is running');
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`maksuds photography is runnig: ${port}`);
 })
